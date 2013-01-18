1c33c08d8129f291f3a2dd5bb84f5d8f76adc944
Robert Reinhard <robert.reinhard@bkwld.com>
2013-01-18 14:07:01 -0800

    Adding json_or_input, a convenience for getting input data

---
+++ Laravel/Input.php
@@ -0,0 +1,16 @@
+<?php namespace BKWLD\Laravel;
+
+// Dependencies
+use Laravel\Request;
+
+class Input {
+	
+	/**
+	 * Get an array of all input from either JSON or POST
+	 */
+	public static function json_or_input() {
+		$json = \Laravel\Input::json(); // Had to save to var to test for empty
+		return Request::ajax() && !empty($json) ? (array) $json : \Laravel\Input::get();
+	}
+	
+}
