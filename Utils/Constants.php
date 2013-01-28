fa04152b911bd49955bc88b32396499e819ef3c5
Robert Reinhard <robert.reinhard@bkwld.com>
2013-01-28 10:17:04 -0800

    JUST date format for MySQL

--- Utils/Constants.php
+++ Utils/Constants.php
@@ -4,6 +4,7 @@
  * A place to put constants, all under the BKWLD namespace
  */
 class Constants {
+	const MYSQL_DATE = "Y-m-d";
 	const MYSQL_DATETIME = "Y-m-d H:i:s";
 	const COMMON_DATE = "n/j/y"; // 3/2/12
 	const COMMON_DATETIME = "n/j/y g:i A"; // 3/2/12 4:12 PM
