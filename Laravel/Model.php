08094bb740d65c6a707ce24b207a03610b2ae514
Robert Reinhard <robert.reinhard@bkwld.com>
2013-01-30 13:25:50 -0800

    Shouldn't have been protected

--- Laravel/Model.php
+++ Laravel/Model.php
@@ -6,7 +6,7 @@ class Model {
 	 * Convert all the model instances in a result set to arrays.  For use in
 	 * making a JSON response from an Eloquent get()
 	 */
-	static protected function eloquent_to_array($query) {
+	static public function eloquent_to_array($query) {
 		return array_map(function($m) { return $m->to_array(); }, $query);
 	}
 	
