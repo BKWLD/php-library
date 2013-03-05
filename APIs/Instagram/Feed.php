2ba7fdc80e8aaebe16bb876a5199b5d466230924
Robert Reinhard <robert.reinhard@bkwld.com>
2013-03-05 12:10:20 -0800

    Adding a feed API for instagram like the other apis have

---
+++ APIs/Instagram/Feed.php
@@ -0,0 +1,59 @@
+<?php namespace BKWLD\APIs\Instagram;
+
+// Dependencies
+use \Laravel\Cache;
+use \Exception;
+use \Laravel\Log;
+use \Laravel\Config;
+
+/**
+ * Load a feed and caches it
+ */
+class Feed {
+	
+	// Settings
+	const CACHE_ID = 'instagram-feed';
+	public static $cache_seconds = 600; // 10 min
+
+	/**
+	 * Load a feed from an open graph resource
+	 * @param $id - An instagram user id
+	 * @param $options - An array that gets merged with the defaults
+	 */
+	public static function user($id, $options = null) {
+		
+		// Return cached response if exists
+		$cache_id = self::CACHE_ID.$id;
+		if (Cache::has($cache_id)) return Cache::get($cache_id);
+		
+		// Default options
+		$defaults = array(
+			'count' => 20,
+			'access_token' => Config::get('instagram.access_token'),
+		);
+		
+		// Apply options
+		if (is_array($options)) $options = array_merge($defaults, $options);
+		$params = http_build_query($options);
+		
+		// Query
+		$url = 'https://api.instagram.com/v1/users/'.$id.'/media/recent?'.$params;
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		if (!($response = curl_exec($ch))) throw new Exception('Feed could not be executed');
+		curl_close($ch);
+		
+		// Verify the response
+		$response = json_decode($response);
+		if ($response->meta->code != 200) throw new Exception('Instagram error:'.$response->meta->error_message);
+		$response = $response->data;
+		
+		// Cache the response
+		Cache::put($cache_id, $response, self::$cache_seconds);
+		
+		// Return the response
+		return $response;
+		
+	}
+	
+}
