0a8be23f00e6b3bbdb6bc470f4fcaef555df6fd2
Robert Reinhard <robert.reinhard@bkwld.com>
2013-03-28 16:03:09 -0700

    Adding a human elapsed time function

--- Utils/String.php
+++ Utils/String.php
@@ -44,4 +44,45 @@ class String {
 		return $url_linker->parse($text, $options);
 	}
 	
+	// Show a human timestamp for how much time has elapssed since the timestamp
+	// Adapted from http://www.zachstronaut.com/posts/2009/01/20/php-relative-date-time-string.html
+	static public function time_elapsed($ptime, $options = null) {
+		
+		// Default options
+		if (!$options) $options = array();
+		$options = array_merge(array(
+			'pluraling' => true,
+			'spacing' => true,
+			'labels' => array(
+				'0 seconds',
+				'second',
+				'minute',
+				'hour',
+				'day',
+				'month',
+				'year',
+			)
+		), $options);
+		
+		$etime = time() - $ptime;
+		if ($etime < 1) { return $options['labels'][0]; }
+		
+		$a = array( 12 * 30 * 24 * 60 * 60  =>  $options['labels'][6],
+		            30 * 24 * 60 * 60       =>  $options['labels'][5],
+		            24 * 60 * 60            =>  $options['labels'][4],
+		            60 * 60                 =>  $options['labels'][3],
+		            60                      =>  $options['labels'][2],
+		            1                       =>  $options['labels'][1],
+		            );
+		
+		foreach ($a as $secs => $str) {
+			$d = $etime / $secs;
+			if ($d >= 1) {
+				$r = round($d);
+				if ($options['pluraling']) return $r . ($options['spacing']?' ':'') . $str . ($r > 1 ? 's' : '');
+				else return $r .($options['spacing']?' ':''). $str;
+			}
+		}
+	}
+	
 }
