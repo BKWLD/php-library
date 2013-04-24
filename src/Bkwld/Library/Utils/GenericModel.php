59c7c9518ee297e56bb220c93f5e788f20e999f1
Robert Reinhard <robert.reinhard@bkwld.com>
2013-04-24 13:26:18 -0700

    Finishing up GenericModel

--- src/Bkwld/Library/Utils/GenericModel.php
+++ src/Bkwld/Library/Utils/GenericModel.php
@@ -5,4 +5,60 @@
  */
 abstract class GenericModel {
 	
+	/**
+	 * This is where the magic attribtues are stored
+	 * @var array
+	 */
+	private $attributes = array();
+	
+	/**
+	 * Set initial attributes on load
+	 */
+	public function __construct(array $attributes = array()) {
+		$this->attributes = $attributes;
+	}
+	
+	/**
+	 * Dynamically retrieve attributes on the model.
+	 *
+	 * @param  string  $key
+	 * @return mixed
+	 */
+	public function __get($key) {
+		if (array_key_exists($key, $this->attributes)) {
+			return $this->attributes[$key];
+		}
+	}
+
+	/**
+	 * Dynamically set attributes on the model.
+	 *
+	 * @param  string  $key
+	 * @param  mixed   $value
+	 * @return void
+	 */
+	public function __set($key, $value) {
+		$this->attributes[$key] = $value;
+	}
+	
+	/**
+	 * Determine if an attribute exists on the model.
+	 *
+	 * @param  string  $key
+	 * @return void
+	 */
+	public function __isset($key) {
+		return isset($this->attributes[$key]);
+	}
+
+	/**
+	 * Unset an attribute on the model.
+	 *
+	 * @param  string  $key
+	 * @return void
+	 */
+	public function __unset($key) {
+		unset($this->attributes[$key]);
+	}
+	
 }
