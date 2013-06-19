d165c1de08ff81509e7749568e0716ed7627daa7
Robert Reinhard <robert.reinhard@bkwld.com>
2013-06-19 16:32:35 -0700

    Adding time constants

--- src/Bkwld/Library/Utils/Constants.php
+++ src/Bkwld/Library/Utils/Constants.php
@@ -6,6 +6,8 @@
 class Constants {
 	const MYSQL_DATE = "Y-m-d";
 	const MYSQL_DATETIME = "Y-m-d H:i:s";
+	const MYSQL_TIME = "H:i:s";
 	const COMMON_DATE = "n/j/y"; // 3/2/12
 	const COMMON_DATETIME = "n/j/y g:i A"; // 3/2/12 4:12 PM
+	const COMMON_TIME = "g:i A"; // 4:12 PM
 }
