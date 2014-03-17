e38a095d3d077820953dd3e9b2ac2c7e24c819a3
Robert Reinhard <robert.reinhard@bkwld.com>
2014-03-17 09:56:57 -0700

    Adding checkboxArray

--- src/Bkwld/Library/Laravel/Former.php
+++ src/Bkwld/Library/Laravel/Former.php
@@ -1,5 +1,8 @@
 <?php namespace Bkwld\Library\Laravel;
 
+// Dependencies
+use App;
+
 /**
  * Functions for working with Former
  */
@@ -23,13 +26,54 @@ class Former {
 	 *   'Pending' => array('value' => 'pending'),
 	 *   'Published' => array('value' => 'published'),
 	 * )
-	 * 
+	 *
+	 * @param  array $src An associative array
+	 * @return array A multidimensional array like Former expects
 	 */
 	static public function radioArray($src) {
 		$out = array();
 		foreach($src as $key => $val) {
 			if (!count($out)) $out[$val] = array('value' => $key, 'checked' => true);
 			else $out[$val] = array('value' => $key);
+		}
+		return $out;
+	}
+
+	/**
+	 * Take a key value array like described for radioArray and make a list of
+	 * checkboxes that are named like an array.  In addition, it auto checks
+	 * boxes on the edit view by looking to the former value
+	 * 
+	 * Thus, if used for the radio example might get an array like:
+	 *
+	 * array(
+	 * 	 'Draft' => array('value' => 'draft', name => 'status[]', checked => false),
+	 *   'Pending' => array('value' => 'pending', name='status[]', checked => true),
+	 *   'Published' => array('value' => 'published', name='status[]', checked => false),
+	 * )
+	 *
+	 * NOTE: You will need to flatten the input to a string manualy.  In Decoy, I do
+	 * this with an onSaving() callback that looks like:
+	 *
+	 * public function onSaving() {
+	 * 	if (is_array($this->status)) {
+	 * 		$this->status = implode(',', array_filter($this->status));
+	 * 	}
+	 * }
+	 *
+	 * @param  string $name The name of the form element (i.e. "status")
+	 * @param  array $src An associative array
+	 * @return array A multidimensional array like Former expects
+	 */
+	static public function checkboxArray($name, $src) {
+		$out = array();
+		$boxes = explode(',', App::make('former')->getValue($name));
+		foreach($src as $key => $val) {
+			$out[$val] = array(
+				'value' => $key,
+				'name' => $name.'[]',
+				'checked' => in_array($key, $boxes),
+			);
 		}
 		return $out;
 	}
