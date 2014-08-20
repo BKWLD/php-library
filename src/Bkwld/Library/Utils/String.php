4ceaf32548259ee2a848ceb40b2b4a1207c5a919
Robert Reinhard <robert.reinhard@bkwld.com>
2014-08-20 09:30:50 -0700

    Changing access

--- src/Bkwld/Library/Utils/String.php
+++ src/Bkwld/Library/Utils/String.php
@@ -16,7 +16,7 @@ class String {
 	 * @param  int $decimals 
 	 * @return string
 	 */
-	private function humanSize($bytes, $decimals = 2) {
+	static public function humanSize($bytes, $decimals = 2) {
 		$sz = 'BKMGTP';
 		$factor = floor((strlen($bytes) - 1) / 3);
 		return sprintf("%.{$decimals}f", $bytes / pow(1024, $factor)) . @$sz[$factor];
