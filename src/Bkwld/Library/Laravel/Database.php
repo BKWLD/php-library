a3ac127b732d4f76973519abe90d4f7ba4d2a88a
Robert Reinhard <robert.reinhard@bkwld.com>
2015-04-07 15:15:47 -0700

    Renaming DB to Database
    Other classes in this package were getting name collisions

--- src/Bkwld/Library/Laravel/Database.php
+++ src/Bkwld/Library/Laravel/Database.php
@@ -2,9 +2,9 @@
 
 // Deps
 use DateTime;
-use DB as LaravelDB;
+use DB;
 
-class DB {
+class Database {
 
 	/**
 	 * Sync up the MySQL timezone with the App timezone.  This assumes
@@ -19,6 +19,6 @@ class DB {
 		$h = $n->getOffset()/3600;
 		$i = 60*($h-floor($h));
 		$offset = sprintf('%+d:%02d', $h, $i);
-		LaravelDB::statement("SET time_zone='$offset'");
+		DB::statement("SET time_zone='$offset'");
 	}
 }
