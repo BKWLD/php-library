63424435d1c3375488447ddcaa797e6be14ccddf
Robert Reinhard <robert.reinhard@bkwld.com>
2017-04-26 09:05:00 -0700

    That PR was merged

--- src/Laravel/Former.php
+++ src/Laravel/Former.php
@@ -78,9 +78,7 @@ class Former {
 	 *
 	 * NOTE: You will need to set `push(false)` on the checkbox to stop hidden
 	 * fields from confusing the repopulation of the form on error.  Otherwise,
-	 * something like this happens: http://cl.ly/image/3l1D32021q2I.  In addition
-	 * you need to use this patch to Former that is currently waiting to be merged:
-	 * https://github.com/Anahkiasen/former/pull/286
+	 * something like this happens: http://cl.ly/image/3l1D32021q2I.
 	 *
 	 * @param  string $name The name of the form element (i.e. "status")
 	 * @param  array $src An associative array
